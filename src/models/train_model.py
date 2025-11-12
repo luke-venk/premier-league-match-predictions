db2f40a51e0fd0752b70f3c45097131da0243f9b
Luke Venkataramanan <luke.venkataramanan@utexas.edu>
2025-11-12 15:28:36 -0600

    feat: create module for training any model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,24 @@
 """
-Train our classifier.
+Train our classifier, depending on the model specified.
 """
+import pandas as pd
+from src.config import Models
+from src.models import logistic_regression
+
+def train(model_type: int, X_train: pd.DataFrame, y_train: pd.Series):
+    """
+    Regardless of the type of model, train the model.
+    
+    Args:
+        df: The dataframe.
+        model_type: The specific type of model we plan to use.
+    
+    Returns:
+        A DataFrame with our data.
+    """
+    if model_type == Models.LOGISTIC_REGRESSION:
+        model = logistic_regression.train_model(X_train, y_train)
+    else:
+        raise ValueError(f'Unknown model type: {model_type}')
+    
+    return model
