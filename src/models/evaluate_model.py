48f0588b63c4fd1a0ace9f38ab1352e211c13a79
Luke Venkataramanan <luke.venkataramanan@utexas.edu>
2025-11-12 15:29:01 -0600

    feat: create module for evaluating model

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -1,3 +1,16 @@
 """
 Evaluate our model using metrics (accuracy, f1, etc.), confusion matrices, etc.
 """
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+def evaluate(model, X_test, y_test):
+    # Predict on holdout set.
+    y_pred  = model.predict(X_test)
+
+    # Print basic statistics.
+    print(f"Accuracy:          {accuracy_score(y_test, y_pred):.3f}")
+    print("\nClassification report (0=H, 1=A, 2=D):")
+    print(classification_report(y_test, y_pred, digits=3))
+    
+    cm = confusion_matrix(y_test, y_pred, labels=[0,1,2])
+    ConfusionMatrixDisplay(cm, display_labels=['Home','Away','Draw']).plot(values_format='d')
